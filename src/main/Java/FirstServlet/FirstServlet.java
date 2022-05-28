cda21bd2159e248f00323b818d26ffc30dbf2eb1
lenovo <radheshyamb9@gmail.com>
2022-05-28 07:21:51 -0700

    ADDED : Creating First Servlet and LoginServlet

---
+++ src/main/Java/FirstServlet/FirstServlet.java
@@ -0,0 +1,21 @@
+package main.Java.FirstServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/FirstServlet")
+public class FirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h3>Hello world My First Servlet!</h3>");
+        out.close();
+    }
+
+}
